b1c44facc78329e68c6cf8170ef4ae36258961ea
sryzzz <1299975117@qq.com>
2022-09-14 21:40:17 +0800

    feat: 封装svg组件，完成登录页布局和初步表单认证

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
+import router from './router';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+// 导入全局样式
+import './style/index.scss';
 
-createApp(App).mount('#app');
+// 导入 svgIcon
+import SvgIcon from './components/SvgIcon/index.vue';
+
+const app = createApp(App);
+app.component('SvgIcon', SvgIcon);
+app.use(ElementPlus).use(router);
+app.mount('#app');
