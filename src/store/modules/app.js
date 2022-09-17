80d771c9939abed875ef22b745785c9548890ea3
sryzzz <1299975117@qq.com>
2022-09-17 11:45:08 +0800

    feat: 创建 tags 数据源

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,4 +1,4 @@
-import { LANG } from '@/constant';
+import { LANG, TAGS_VIEW } from '@/constant';
 import { getItem, setItem } from '@/utils/storage';
 
 export default {
@@ -6,6 +6,7 @@ export default {
   state: () => ({
     sidebarOpened: true,
     language: getItem(LANG) || 'zh_CN',
+    tagsViewList: getItem(TAGS_VIEW) || [],
   }),
   mutations: {
     triggerSidebarOpened(state) {
@@ -17,6 +18,19 @@ export default {
     setLanguage(state, lang) {
       setItem(LANG, lang);
       state.language = lang;
+    },
+    /**
+     * 添加 tags
+     */
+    addTagsViewList(state, tag) {
+      const isFind = state.tagsViewList.find((item) => {
+        return item.path === tag.path;
+      });
+      // 处理重复
+      if (!isFind) {
+        state.tagsViewList.push(tag);
+        setItem(TAGS_VIEW, state.tagsViewList);
+      }
     },
   },
   actions: {},
