89635a0c4f0ec93b71ede72a8907c493e4c5cb8c
sryzzz <1299975117@qq.com>
2022-09-17 11:30:55 +0800

    feat: 完成 自定义主题

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -6,7 +6,7 @@
     <!-- 左侧 menu -->
     <sidebar
       class="sidebar-container"
-      :style="{ backgroundColor: variables.menuBg }"
+      :style="{ backgroundColor: $store.getters.cssVar.menuBg }"
     />
     <div class="main-container">
       <div class="fixed-header">
@@ -23,8 +23,6 @@
 import Navbar from './components/Navbar.vue';
 import Sidebar from './components/Sidebar/index.vue';
 import AppMain from './components/AppMain.vue';
-import variables from '@/style/variables.module.scss';
-import {} from 'vue';
 </script>
 
 <style lang="scss" scoped>
