b83380e67e8059171c1d16c29e309a3e437bed59
sryzzz <1299975117@qq.com>
2022-09-18 23:13:34 +0800

    feat: 实现TagsView右键菜单

---
+++ src/components/TagsView/ContextMenu.vue
@@ -0,0 +1,81 @@
+<template>
+  <ul class="context-menu-container">
+    <li @click="onRefreshClick">
+      {{ $t('msg.tagsView.refresh') }}
+    </li>
+    <li @click="onCloseRightClick">
+      {{ $t('msg.tagsView.closeRight') }}
+    </li>
+    <li @click="onCloseOtherClick">
+      {{ $t('msg.tagsView.closeOther') }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+
+const props = defineProps({
+  index: {
+    type: Number,
+    required: true,
+  },
+});
+
+/**
+ * 刷新
+ * @type {Router}
+ */
+const router = useRouter();
+const onRefreshClick = () => {
+  router.go(0);
+};
+
+/**
+ * 关闭右侧
+ * @type {Store<any>}
+ */
+const store = useStore();
+const onCloseRightClick = () => {
+  store.commit('app/removeTagsView', {
+    type: 'right',
+    index: props.index,
+  });
+};
+
+/**
+ * 关闭其他
+ */
+const onCloseOtherClick = () => {
+  store.commit('app/removeTagsView', {
+    type: 'other',
+    index: props.index,
+  });
+};
+</script>
+
+<style lang="scss" scoped>
+.context-menu-container {
+  position: fixed;
+  background: #fff;
+  z-index: 3000;
+  list-style-type: none;
+  padding: 5px 0;
+  border-radius: 4px;
+  font-size: 12px;
+  font-weight: 400;
+  color: #333;
+  box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, 0.3);
+
+  li {
+    margin: 0;
+    padding: 7px 16px;
+    cursor: pointer;
+
+    &:hover {
+      background: #eee;
+    }
+  }
+}
+</style>
--- src/components/TagsView/index.vue
+++ src/components/TagsView/index.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="tags-view-container">
-    <router-link
-      v-for="(tag, index) in $store.getters.tagsViewList"
-      :key="tag.fullPath"
-      class="tags-view-item"
-      :class="isActive(tag) ? 'active' : ''"
-      :style="{
-        backgroundColor: isActive(tag) ? $store.getters.cssVar.menuBg : '',
-        borderColor: isActive(tag) ? $store.getters.cssVar.menuBg : '',
-      }"
-      :to="{ path: tag.fullPath }"
-    >
-      {{ tag.title }}
-      <el-icon
-        v-show="!isActive(tag)"
-        class="el-icon-close"
-        @click.prevent.stop="onCloseClick(index)"
+    <el-scrollbar class="tags-view-wrapper">
+      <router-link
+        v-for="(tag, index) in $store.getters.tagsViewList"
+        :key="tag.fullPath"
+        class="tags-view-item"
+        :class="isActive(tag) ? 'active' : ''"
+        :style="{
+          backgroundColor: isActive(tag) ? $store.getters.cssVar.menuBg : '',
+          borderColor: isActive(tag) ? $store.getters.cssVar.menuBg : '',
+        }"
+        :to="{ path: tag.fullPath }"
+        @contextmenu.prevent="openMenu($event, index)"
       >
-        <CloseBold />
-      </el-icon>
-    </router-link>
+        {{ tag.title }}
+        <el-icon
+          v-show="!isActive(tag)"
+          class="el-icon-close"
+          @click.prevent.stop="onCloseClick(index)"
+        >
+          <CloseBold />
+        </el-icon>
+      </router-link>
+    </el-scrollbar>
+    <context-menu
+      v-show="visible"
+      :style="menuStyle"
+      :index="selectIndex"
+    ></context-menu>
   </div>
 </template>
 
 <script setup>
+import { ref, reactive, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import { useStore } from 'vuex';
+import ContextMenu from './ContextMenu.vue';
 
 const route = useRoute();
 
@@ -38,9 +49,50 @@ const isActive = (tag) => {
 /**
  * 关闭 tag 的点击事件
  */
+const store = useStore();
 const onCloseClick = (index) => {
   console.log(index);
+  store.commit('app/removeTagsView', {
+    type: 'index',
+    index: index,
+  });
 };
+
+// contextMenu 相关
+const selectIndex = ref(0);
+const visible = ref(false);
+const menuStyle = reactive({
+  left: 0,
+  top: 0,
+});
+/**
+ * 展示 menu
+ */
+const openMenu = (e, index) => {
+  const { x, y } = e;
+  menuStyle.left = x + 'px';
+  menuStyle.top = y + 'px';
+  selectIndex.value = index;
+  visible.value = true;
+};
+
+/**
+ * 关闭 menu
+ */
+const closeMenu = () => {
+  visible.value = false;
+};
+
+/**
+ * 监听变化
+ */
+watch(visible, (val) => {
+  if (val) {
+    document.body.addEventListener('click', closeMenu);
+  } else {
+    document.body.removeEventListener('click', closeMenu);
+  }
+});
 </script>
 
 <style lang="scss" scoped>
